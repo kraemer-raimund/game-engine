a747af1021b358c1801796195bb23ccc29914f52
Raimund Kraemer <kraemer_raimund@web.de>
2024-03-29 20:25:55 +0100

    Organize Math tests hierarchically with nested classes

--- Engine/src/test/kotlin/dev/rakrae/gameengine/math/MathTest.kt
+++ Engine/src/test/kotlin/dev/rakrae/gameengine/math/MathTest.kt
@@ -1,21 +1,27 @@
 package dev.rakrae.gameengine.math
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@DisplayName("Math")
 internal class MathTest {
 
-    @Test
-    fun `clamping value within range preserves the value`() {
-        val clamped = clamp(42.0f, 30.0f, 50.0f)
+    @Nested
+    @DisplayName("clamping value")
+    inner class Clamp {
 
-        assertEquals(clamped, 42.0f)
-    }
+        @Test
+        fun `within range preserves the value`() {
+            val clamped = clamp(42.0f, 30.0f, 50.0f)
+            assertEquals(clamped, 42.0f)
+        }
 
-    @Test
-    fun `clamping value less than min value results in min value`() {
-        val clamped = clamp(13.0f, 30.0f, 50.0f)
-
-        assertEquals(clamped, 30.0f)
+        @Test
+        fun `less than min value results in min value`() {
+            val clamped = clamp(13.0f, 30.0f, 50.0f)
+            assertEquals(clamped, 30.0f)
+        }
     }
 }
