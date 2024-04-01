27b430e47231f6ce112e2dd60c47f2b181d49c17
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-01 20:55:07 +0200

    Replace std lib math calls with use of custom math library

--- Engine/src/main/kotlin/dev/rakrae/gameengine/math/Math.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/math/Math.kt
@@ -1,16 +1,13 @@
 package dev.rakrae.gameengine.math
 
-import kotlin.math.max
-import kotlin.math.min
-
-fun min(v1: Float, v2: Float): Any {
+fun min(v1: Float, v2: Float): Float {
     return if (v2 < v1) v2 else v1
 }
 
-fun max(v1: Float, v2: Float): Any {
+fun max(v1: Float, v2: Float): Float {
     return if (v2 > v1) v2 else v1
 }
 
-fun clamp(value: Float, minValue: Float, maxValue:Float): Float {
+fun clamp(value: Float, minValue: Float, maxValue: Float): Float {
     return min(maxValue, max(minValue, value))
 }
