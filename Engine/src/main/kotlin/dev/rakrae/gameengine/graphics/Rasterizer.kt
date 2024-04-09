3276d53b08093127e9103c3271eb00fcf53e37ea
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-09 12:13:03 +0200

    Fill half of triangles using line sweeping

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/Rasterizer.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/Rasterizer.kt
@@ -9,6 +9,20 @@ class Rasterizer {
     fun render(mesh: Mesh, image: Bitmap) {
         for (triangle in mesh.triangles) {
             drawWireframe(triangle, image)
+            drawFilled(
+                triangle = projectToScreenCoordinates(triangle, screenSize = Vec2i(image.width, image.height)),
+                image = image
+            )
+        }
+    }
+
+    private fun projectToScreenCoordinates(triangle: Triangle, screenSize: Vec2i): List<Vec2i> {
+        val vertices = listOf(triangle.v1, triangle.v2, triangle.v3)
+        return vertices.map {
+            Vec2i(
+                ((it.position.z + 1) * screenSize.x / 6f).toInt(),
+                ((it.position.y + 1) * screenSize.y / 6f).toInt()
+            )
         }
     }
 
@@ -30,20 +44,57 @@ class Rasterizer {
     }
 
     private fun drawFilled(triangle: List<Vec2i>, image: Bitmap) {
-        TODO("Not completely implemented, but not called yet.")
         // Line sweeping algorithm.
 
         // Sort vertices by y coordinate (i.e., vertically, lowest-to-highest).
         val pixels = triangle.sortedBy { it.y }
+        val p0 = pixels[0]
+        val p1 = pixels[1]
+        val p2 = pixels[2]
 
         // Determine height of the triangle: Difference between the highest and lowest points.
-        val triangleHeight = pixels[2].y - pixels[0].y
+        val triangleHeight = p2.y - p0.y
 
         // Split the triangle horizontally into 2 segments, where the vertex between the lowest
         // and the highest determines the splitting line.
-        val splittingHeight = pixels[1].y
-        for (y in pixels[0].y..<splittingHeight) {
+        for (y in p0.y..<p1.y) {
+            val segmentHeight = p1.y - p0.y + 1
 
+            /**
+             * The ratio of the current y coordinate between the lowest and the highest points
+             * of the whole triangle. We use this ratio to scale a vector along the **longer** side
+             * of the triangle.
+             */
+            val a = (y - p0.y) / triangleHeight.toFloat()
+
+            /**
+             * The ratio of the current y coordinate between the lowest point of the triangle
+             * and the "middle" corner on either side of the triangle. We use this ratio
+             * to scale a vector along the **shorter** side of the triangle.
+             */
+            val b = (y - p0.y) / segmentHeight.toFloat()
+
+            /**
+             * The vector along the longer side of the triangle, pointing from the lowest corner
+             * to the "current" y coordinate on the longer side, moving towards the top corner
+             * of the triangle as we increment y.
+             */
+            val vecLong = p0 + Vec2i(
+                ((p2 - p0).x * a).toInt(),
+                ((p2 - p0).y * a).toInt()
+            )
+
+            /**
+             * The vector along the shorter side of the triangle, pointing from the lowest corner
+             * to the "current" y coordinate on the shorter side, moving towards the middle corner
+             * of the triangle as we increment y.
+             */
+            val vecShort = p0 + Vec2i(
+                ((p1 - p0).x * b).toInt(),
+                ((p1 - p0).y * b).toInt()
+            )
+
+            drawLine(vecLong, vecShort, image)
         }
     }
 
