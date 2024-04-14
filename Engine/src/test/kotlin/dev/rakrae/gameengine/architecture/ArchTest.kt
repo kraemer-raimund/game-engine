0daf83e2f5dfcdd47304bac17034f6d08af81658
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-14 21:18:08 +0200

    Remove architecture rule to restrict math lib usage
    We can easily recreate some of the math library functionality, but
    some of the more complicated functions are not worth the effort. So
    we might recreate some of the math functions for fun and practice,
    but it's ok to use the standard library math for, e.g., trigonometric
    functions etc.

--- Engine/src/test/kotlin/dev/rakrae/gameengine/architecture/ArchTest.kt
+++ Engine/src/test/kotlin/dev/rakrae/gameengine/architecture/ArchTest.kt
@@ -4,7 +4,6 @@ import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTag
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.ArchRule
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
 import com.tngtech.archunit.library.GeneralCodingRules
 import dev.rakrae.gameengine.TestTag
 
@@ -14,14 +13,4 @@ class ArchTest {
 
     @ArchTest
     private val noGenericExceptions: ArchRule = GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS
-
-    /**
-     * We want to intentionally reinvent some wheels, including creating our
-     * own math "library" for this game engine, implementing the math we need
-     * when we need it.
-     */
-    @ArchTest
-    val noMathFromStandardLibrary: ArchRule =
-        noClasses().that().resideInAPackage("dev.rakrae.gameengine.*")
-            .should().dependOnClassesThat().haveNameMatching("java.lang.Math")
 }
