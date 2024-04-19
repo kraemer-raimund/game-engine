1ff5802e504ecb48dab57a0e30d967aa7bde3b73
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-19 22:26:07 +0200

    Load multiple meshes at once

--- SampleGame/src/main/kotlin/dev/rakrae/gameengine/samplegame/SampleGame.kt
+++ SampleGame/src/main/kotlin/dev/rakrae/gameengine/samplegame/SampleGame.kt
@@ -8,8 +8,11 @@ class SampleGame : Game {
     override val title = "Chess (Sample Game)"
 
     override val meshes by lazy {
-        val chessPieces = AssetLoader().loadMesh("/assets/chesspieces/chess-piece-3d-models.obj")
-        sequenceOf(chessPieces)
+        val king = AssetLoader().loadMesh("/assets/chesspieces/king.obj")
+        val queen = AssetLoader().loadMesh("/assets/chesspieces/queen.obj")
+        val bishop = AssetLoader().loadMesh("/assets/chesspieces/bishop.obj")
+        val knight = AssetLoader().loadMesh("/assets/chesspieces/knight.obj")
+        sequenceOf(king, queen, bishop, knight)
     }
 
     override fun onTick() {
