a97bb6175c4089ddaec7cffed9e0298e24996aaa
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-19 23:50:23 +0200

    Clamp AABB within another AABB

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/AABB2i.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/AABB2i.kt
@@ -1,12 +1,31 @@
 package dev.rakrae.gameengine.graphics
 
 import dev.rakrae.gameengine.math.Vec2i
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * An axis-aligned bounding box (AABB) in 2 dimensions with integer coordinates, as used for
  * example for the rectangle of pixels around a set of points on the screen.
  */
 data class AABB2i(val min: Vec2i, val max: Vec2i) {
+
+    fun clampWithin(bounds: AABB2i): AABB2i {
+        return AABB2i(
+            min = Vec2i(
+                x = clamp(this.min.x, bounds.min.x, bounds.max.x),
+                y = clamp(this.min.y, bounds.min.y, bounds.max.y)
+            ),
+            max = Vec2i(
+                x = clamp(this.max.x, bounds.min.x, bounds.max.x),
+                y = clamp(this.max.y, bounds.min.y, bounds.max.y)
+            )
+        )
+    }
+
+    private fun clamp(value: Int, min: Int, max: Int): Int {
+        return min(max, max(value, min))
+    }
 
     companion object {
         fun calculateBoundingBox(points: Iterable<Vec2i>): AABB2i {
--- Engine/src/test/kotlin/dev/rakrae/gameengine/graphics/AABBTest.kt
+++ Engine/src/test/kotlin/dev/rakrae/gameengine/graphics/AABBTest.kt
@@ -5,6 +5,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 @DisplayName("AABB (Axis-aligned bounding box)")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -26,5 +31,53 @@ internal class AABBTest {
                     Vec2i(900, 0),
                 )
             )
+    }
+
+    private fun clampWithinOtherTestArgs(): Stream<Arguments> {
+        return Stream.of(
+            arguments(
+                "Completely enclosed by the bounds, result is the original AABB.",
+                AABB2i(Vec2i(-2, -2), Vec2i(2, 2)),
+                AABB2i(Vec2i(-40, -40), Vec2i(40, 40)),
+                AABB2i(Vec2i(-2, -2), Vec2i(2, 2))
+            ),
+            arguments(
+                "Completely enclosing the bounds, gets clamped to the bounds.",
+                AABB2i(Vec2i(-20, -20), Vec2i(20, 20)),
+                AABB2i(Vec2i(-13, -13), Vec2i(7, 7)),
+                AABB2i(Vec2i(-13, -13), Vec2i(7, 7))
+            ),
+            arguments(
+                "Overlapping with the bounds, gets partially clamped to the bounds.",
+                AABB2i(Vec2i(-20, -20), Vec2i(20, 20)),
+                AABB2i(Vec2i(-13, -13), Vec2i(70, 70)),
+                AABB2i(Vec2i(-13, -13), Vec2i(20, 20))
+            ),
+            arguments(
+                "Completely outside of the bounds, gets clamped to closest edge.",
+                AABB2i(Vec2i(-200, -200), Vec2i(-100, 200)),
+                AABB2i(Vec2i(100, -100), Vec2i(200, 70)),
+                AABB2i(Vec2i(100, -100), Vec2i(100, 70))
+            ),
+            arguments(
+                "Completely outside of the bounds, gets clamped to closest corner.",
+                AABB2i(Vec2i(1337, 1337), Vec2i(1342, 1342)),
+                AABB2i(Vec2i(10, 10), Vec2i(20, 20)),
+                AABB2i(Vec2i(20, 20), Vec2i(20, 20))
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("clampWithinOtherTestArgs")
+    fun `clamps this AABB within bounds of another AABB`(
+        description: String,
+        toClamp: AABB2i,
+        bounds: AABB2i,
+        expected: AABB2i
+    ) {
+        assertThat(toClamp.clampWithin(bounds))
+            .isEqualTo(expected)
+            .describedAs(description)
     }
 }
