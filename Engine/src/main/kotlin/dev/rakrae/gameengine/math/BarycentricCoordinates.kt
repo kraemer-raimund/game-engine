1734b7b0c4fe98495721e94a663a6a61894f76f5
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-20 11:33:38 +0200

    Add class for barycentric coordinates

---
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/math/BarycentricCoordinates.kt
@@ -0,0 +1,12 @@
+package dev.rakrae.gameengine.math
+
+/**
+ * Barycentric coordinates describe a point relative to a triangle's corners.
+ * Intuitively, they assign a weight to each of the 3 vertices, where the resulting
+ * "center of mass" is the point P.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Barycentric_coordinate_system">Barycentric coordinates (Wikipedia)</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Barycentric_coordinate_system#Conversion_between_barycentric_and_Cartesian_coordinates">Conversion between barycentric and Cartesian coordinates (Wikipedia)</a>
+ */
+class BarycentricCoordinates {
+}
