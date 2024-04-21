01abbc389dc2f9ac7058f2dd9aad340858a31c08
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-21 20:00:40 +0200

    Experiment with fragment shaders

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/pipeline/DummyExampleFragmentShader.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/pipeline/DummyExampleFragmentShader.kt
@@ -1,8 +1,7 @@
 package dev.rakrae.gameengine.graphics.pipeline
 
 import dev.rakrae.gameengine.graphics.Color
-import kotlin.random.Random
-import kotlin.random.nextUInt
+import kotlin.math.pow
 
 /**
  * Used during development to get some visual feedback. Will be deleted in the future, and shaders
@@ -11,10 +10,29 @@ import kotlin.random.nextUInt
 class DummyExampleFragmentShader : FragmentShader {
 
     override fun process(fragment: Fragment): Fragment {
+        val glow = Color(
+            glow(fragment.color.r, 1.2f, 4.4f),
+            glow(fragment.color.g, 0.4f, 2.8f),
+            glow(fragment.color.b, 0.4f, 2.8f),
+            255u
+        )
         return Fragment(
             screenPosition = fragment.screenPosition,
-            color = Color.fromIntARGB(Random.nextUInt()),
+            color = glow,
             depth = fragment.depth
         )
+    }
+
+    private fun glow(
+        value: UByte,
+        factor: Float,
+        exponent: Float
+    ): UByte {
+        val glowValueNormalized = (value.normalized() * factor).pow(exponent).coerceIn(0f..1f)
+        return (glowValueNormalized * 255).toInt().toUByte()
+    }
+
+    private fun UByte.normalized(): Float {
+        return toInt() / 255f
     }
 }
