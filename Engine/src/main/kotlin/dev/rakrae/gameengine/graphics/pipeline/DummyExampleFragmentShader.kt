f63edf155cddeadd473084964bd9bbc1dd9e138c
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-21 20:09:30 +0200

    Experiment with shaders - night vision effect

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/pipeline/DummyExampleFragmentShader.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/pipeline/DummyExampleFragmentShader.kt
@@ -2,6 +2,7 @@ package dev.rakrae.gameengine.graphics.pipeline
 
 import dev.rakrae.gameengine.graphics.Color
 import kotlin.math.pow
+import kotlin.random.Random
 
 /**
  * Used during development to get some visual feedback. Will be deleted in the future, and shaders
@@ -11,8 +12,8 @@ class DummyExampleFragmentShader : FragmentShader {
 
     override fun process(fragment: Fragment): Fragment {
         val glow = Color(
-            glow(fragment.color.r, 1.2f, 4.4f),
-            glow(fragment.color.g, 0.4f, 2.8f),
+            glow(fragment.color.r, 0.4f, 2.8f),
+            glow(fragment.color.g, Random.nextFloat() * 0.4f + 0.9f, 4.4f),
             glow(fragment.color.b, 0.4f, 2.8f),
             255u
         )
@@ -28,7 +29,9 @@ class DummyExampleFragmentShader : FragmentShader {
         factor: Float,
         exponent: Float
     ): UByte {
-        val glowValueNormalized = (value.normalized() * factor).pow(exponent).coerceIn(0f..1f)
+        val glowValueNormalized = (value.normalized() * factor)
+            .pow(exponent)
+            .coerceIn(0f..1.0f)
         return (glowValueNormalized * 255).toInt().toUByte()
     }
 
