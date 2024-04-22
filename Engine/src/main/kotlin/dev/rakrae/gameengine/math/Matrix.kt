37c5f2bf4721e4e2238e0885933d520f49e4f0d4
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-22 20:34:02 +0200

    Add type for 4x4 matrix

---
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/math/Matrix.kt
@@ -0,0 +1,8 @@
+package dev.rakrae.gameengine.math
+
+data class Mat4x4f(
+    val a11: Float, val a12: Float, val a13: Float, val a14: Float,
+    val a21: Float, val a22: Float, val a23: Float, val a24: Float,
+    val a31: Float, val a32: Float, val a33: Float, val a34: Float,
+    val a41: Float, val a42: Float, val a43: Float, val a44: Float
+)
