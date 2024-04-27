19f219627e9e2110647b19fcbc39d2f4926408ba
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-27 20:14:10 +0200

    Remove by lazy from trivial calculation

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/Triangle.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/Triangle.kt
@@ -4,10 +4,11 @@ import dev.rakrae.gameengine.math.Vec3f
 
 class Triangle(val v0: Vertex, val v1: Vertex, val v2: Vertex) {
 
-    val normal: Vec3f by lazy {
-        val a = v0.position.toVec3f()
-        val b = v1.position.toVec3f()
-        val c = v2.position.toVec3f()
-        return@lazy (c - a) cross (b - a)
-    }
+    val normal: Vec3f
+        get() {
+            val a = v0.position.toVec3f()
+            val b = v1.position.toVec3f()
+            val c = v2.position.toVec3f()
+            return (c - a) cross (b - a)
+        }
 }
