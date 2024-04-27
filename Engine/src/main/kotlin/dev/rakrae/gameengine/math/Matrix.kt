b1760c9d59e4e2d14729bd4634ab498e2db3c0af
Raimund Kraemer <kraemer_raimund@web.de>
2024-04-27 19:45:10 +0200

    Matrix addition for 4x4 float matrices

--- Engine/src/main/kotlin/dev/rakrae/gameengine/math/Matrix.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/math/Matrix.kt
@@ -1,8 +1,40 @@
 package dev.rakrae.gameengine.math
 
+import kotlin.math.abs
+
 data class Mat4x4f(
     val a11: Float, val a12: Float, val a13: Float, val a14: Float,
     val a21: Float, val a22: Float, val a23: Float, val a24: Float,
     val a31: Float, val a32: Float, val a33: Float, val a34: Float,
     val a41: Float, val a42: Float, val a43: Float, val a44: Float
-)
+) {
+
+    private val asList
+        get() = listOf(
+            a11, a12, a13, a14,
+            a21, a22, a23, a24,
+            a31, a32, a33, a34,
+            a41, a42, a43, a44
+        )
+
+    private constructor(m: List<Float>) : this(
+        m[0], m[1], m[2], m[3],
+        m[4], m[5], m[6], m[7],
+        m[8], m[9], m[10], m[11],
+        m[12], m[13], m[14], m[15]
+    )
+
+    operator fun plus(matrix: Mat4x4f): Mat4x4f {
+        val thisAsArray = this.asList
+        val otherAsArray = matrix.asList
+        val sumAsArray = (0..15).map { i -> thisAsArray[i] + otherAsArray[i] }
+        return Mat4x4f(sumAsArray)
+    }
+
+    fun isCloseTo(matrix: Mat4x4f, epsilon: Float = 0.01f): Boolean {
+        val similar = { f1: Float, f2: Float -> abs(f1 - f2) < epsilon }
+        val thisAsArray = this.asList
+        val otherAsArray = matrix.asList
+        return (0..15).all { i -> similar(thisAsArray[i], otherAsArray[i]) }
+    }
+}
--- Engine/src/test/kotlin/dev/rakrae/gameengine/math/LinearAlgebraTest.kt
+++ Engine/src/test/kotlin/dev/rakrae/gameengine/math/LinearAlgebraTest.kt
@@ -152,4 +152,40 @@ internal class LinearAlgebraTest {
             assertTrue(v.normalized.isCloseTo(expectedNormalized, epsilon = 0.01f))
         }
     }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class Mat4x4fTest {
+
+        @Test
+        fun `matrix addition`() {
+            val m1 = Mat4x4f(
+                a11 = 3.2f, a12 = 4.1f, a13 = 11.7f, a14 = -11.89f,
+                a21 = 42.69f, a22 = 13.37f, a23 = 1f, a24 = 0.9f,
+                a31 = -2.789f, a32 = -6.456f, a33 = 8f, a34 = 4.5f,
+                a41 = 7.654f, a42 = 4.357f, a43 = 8.47f, a44 = 6.894f
+            )
+            val m2 = Mat4x4f(
+                a11 = 31.2f, a12 = 4.7f, a13 = 13.4f, a14 = 11.62f,
+                a21 = -2.689f, a22 = 13.42f, a23 = 12.7f, a24 = 0.9f,
+                a31 = 2.51f, a32 = 6.456f, a33 = 18.56f, a34 = -54.5f,
+                a41 = -7f, a42 = 3.57f, a43 = 81.87f, a44 = -6.894f
+            )
+            val expectedSum = Mat4x4f(
+                a11 = 34.4f, a12 = 8.8f, a13 = 25.1f, a14 = -0.27f,
+                a21 = 40.001f, a22 = 26.79f, a23 = 13.7f, a24 = 1.8f,
+                a31 = -0.279f, a32 = 0.0f, a33 = 26.56f, a34 = -50.0f,
+                a41 = 0.654f, a42 = 7.927f, a43 = 90.34f, a44 = 0.0f
+            )
+
+            val actualSum = m1 + m2
+
+            assertTrue(
+                actualSum.isCloseTo(expectedSum),
+                "Expected matrices to be equal (within margin for rounding error).\n" +
+                        "Expected:\n$expectedSum\n" +
+                        "Actual:\n$actualSum"
+            )
+        }
+    }
 }
