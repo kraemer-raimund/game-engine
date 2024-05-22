cba046c2d0d941cab54a0d3af11cecc6a30df6b3
Raimund Kraemer <kraemer_raimund@web.de>
2024-05-22 17:43:45 +0200

    Create DoubleBufferedBitmap class

---
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/DoubleBufferedBitmap.kt
@@ -0,0 +1,39 @@
+package dev.rakrae.gameengine.graphics
+
+/**
+ * Behaves similar to a bitmap, but with separate read and write buffers. This prevents situations
+ * where, e.g., one camera reads from a texture that is only partially rendered/loaded/cleared, etc.
+ * Similarly, it can prevent race conditions in case of multi-threading, as long as the buffer swap
+ * is synchronized.
+ *
+ * The *front buffer* is the one used for reading. The *back buffer* is used for writing.
+ * The *buffer swap* makes the pixels previously written to the back buffer available for any
+ * reading threads/cameras/etc.
+ */
+class DoubleBufferedBitmap(
+    val width: Int,
+    val height: Int,
+    initialColor: Color = Color.black
+) {
+
+    private var frontBuffer = Bitmap(width, height, initialColor)
+    private var backBuffer = Bitmap(width, height, initialColor)
+
+    /**
+     * Write a pixel to the back buffer.
+     */
+    fun writePixel(x: Int, y: Int, color: Color) {
+        backBuffer.setPixel(x, y, color)
+    }
+
+    /**
+     * Read a pixel from the front buffer.
+     */
+    fun readPixel(x: Int, y: Int): Color {
+        return frontBuffer.getPixel(x, y)
+    }
+
+    fun swap() {
+        frontBuffer = backBuffer.also { backBuffer = frontBuffer }
+    }
+}
---
+++ Engine/src/test/kotlin/dev/rakrae/gameengine/graphics/DoubleBufferTest.kt
@@ -0,0 +1,27 @@
+package dev.rakrae.gameengine.graphics
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class DoubleBufferTest {
+
+    @Test
+    fun `reads and writes use different buffers before buffer swap`() {
+        val doubleBuffer = DoubleBufferedBitmap(width = 69, height = 42, initialColor = Color.black)
+
+        doubleBuffer.writePixel(13, 37, Color.blue)
+
+        assertThat(doubleBuffer.readPixel(13, 37)).isEqualTo(Color.black)
+    }
+
+    @Test
+    fun `after buffer swap the previously written pixels become available for reading`() {
+        val doubleBuffer = DoubleBufferedBitmap(width = 21, height = 12, initialColor = Color.white)
+
+        doubleBuffer.writePixel(4, 7, Color.blue)
+        doubleBuffer.swap()
+
+        assertThat(doubleBuffer.readPixel(4, 7)).isEqualTo(Color.blue)
+        assertThat(doubleBuffer.readPixel(5, 2)).isEqualTo(Color.white)
+    }
+}
