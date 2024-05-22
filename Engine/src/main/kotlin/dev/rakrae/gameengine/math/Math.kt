50d55864595c89233f4fdb10aa3223c186d12c46
Raimund Kraemer <kraemer_raimund@web.de>
2024-05-22 18:55:58 +0200

    Inline companion object of Math

--- Engine/src/main/kotlin/dev/rakrae/gameengine/math/Math.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/math/Math.kt
@@ -1,12 +1,10 @@
 package dev.rakrae.gameengine.math
 
-class Math {
+object Math {
 
-    companion object {
-        fun lerp(v1: Float, v2: Float, t: Float): Float {
-            t.coerceIn(0f..1f).let { weight ->
-                return v1 * (1f - weight) + v2 * weight
-            }
+    fun lerp(v1: Float, v2: Float, t: Float): Float {
+        t.coerceIn(0f..1f).let { weight ->
+            return v1 * (1f - weight) + v2 * weight
         }
     }
 }
