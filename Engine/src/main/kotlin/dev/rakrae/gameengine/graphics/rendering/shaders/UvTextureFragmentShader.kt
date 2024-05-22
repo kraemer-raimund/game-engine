69fc0f1b551eb249a4985808497fecaa29b858c1
Raimund Kraemer <kraemer_raimund@web.de>
2024-05-22 19:31:45 +0200

    Implement bilinear texture filtering
    For now we do this directly in the fragment shader. Later on, we can
    move this into a separate class (e.g., TextureSampler, maybe some of
    the logic for color interpolation directly into the color).
    
    The choice is between "nearest" and "bilinear" filtering. "Nearest"
    rounds the UV coordinate to the nearest texel in the texture. This is
    very fast. "Bilinear" bilinearly interpolates between the 4 pixels
    surrounding the current UV coordinates, using the distance from each
    of those 4 pixels as the interpolation weights.
    
    We are not handling sub-pixel precision, and we are using the pixels'
    bottom-left corner rather than their center for simplicity.

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/rendering/shaders/UvTextureFragmentShader.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/rendering/shaders/UvTextureFragmentShader.kt
@@ -8,7 +8,10 @@ import dev.rakrae.gameengine.graphics.rendering.pipeline.InputFragment
 import dev.rakrae.gameengine.graphics.rendering.pipeline.OutputFragment
 import dev.rakrae.gameengine.math.Vec3f
 import dev.rakrae.gameengine.math.Vec4f
+import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.pow
+import kotlin.math.round
 
 class UvTextureFragmentShader : FragmentShader {
 
@@ -73,11 +76,58 @@ class UvTextureFragmentShader : FragmentShader {
             val uvScale = inputFragment.material.uvScale
             val x = uvScale.x * uv.x * (texture.width - 1)
             val y = uvScale.y * uv.y * (texture.height - 1)
-            texture.getPixel(
-                x = x.toInt().mod(texture.width - 1),
-                y = y.toInt().mod(texture.height - 1)
-            )
+            when (interpolation) {
+                Interpolation.BILINEAR -> interpolateBilinear(x, y, texture)
+                else -> interpolateNearest(x, y, texture)
+            }
         }
+    }
+
+    private fun interpolateNearest(
+        x: Float,
+        y: Float,
+        texture: Bitmap
+    ): Color {
+        val xInterpolated = round(x).toInt().mod(texture.width - 1)
+        val yInterpolated = round(y).toInt().mod(texture.height - 1)
+        return texture.getPixel(xInterpolated, yInterpolated)
+    }
+
+    private fun interpolateBilinear(
+        x: Float,
+        y: Float,
+        texture: Bitmap
+    ): Color {
+        val xLow = floor(x).toInt()
+        val xHigh = ceil(x).toInt()
+        val yLow = floor(y).toInt()
+        val yHigh = ceil(y).toInt()
+
+        val colorBottomLeft = texture.getPixel(
+            xLow.mod(texture.width - 1),
+            yLow.mod(texture.height - 1)
+        )
+        val colorBottomRight = texture.getPixel(
+            xHigh.mod(texture.width - 1),
+            yLow.mod(texture.height - 1)
+        )
+        val colorTopLeft = texture.getPixel(
+            xLow.mod(texture.width - 1),
+            yHigh.mod(texture.height - 1)
+        )
+        val colorTopRight = texture.getPixel(
+            xHigh.mod(texture.width - 1),
+            yHigh.mod(texture.height - 1)
+        )
+
+        val weightX = x - xLow
+        val weightY = y - yLow
+        val biLerpedColor = lerp(
+            lerp(colorBottomLeft, colorBottomRight, weightX),
+            lerp(colorTopLeft, colorTopRight, weightX),
+            weightY
+        )
+        return biLerpedColor
     }
 
     private fun Color.toNormal(): Vec3f {
@@ -95,5 +145,26 @@ class UvTextureFragmentShader : FragmentShader {
             (value * b.toInt()).toInt().toUByte(),
             255u
         )
+    }
+
+    private fun lerp(color1: Color, color2: Color, t: Float): Color {
+        val r = (1 - t) * color1.r.toInt() + t * color2.r.toInt()
+        val g = (1 - t) * color1.g.toInt() + t * color2.g.toInt()
+        val b = (1 - t) * color1.b.toInt() + t * color2.b.toInt()
+        return Color(
+            (r.toInt().coerceIn(0, 255)).toUByte(),
+            (g.toInt().coerceIn(0, 255)).toUByte(),
+            (b.toInt().coerceIn(0, 255)).toUByte(),
+            255u
+        )
+    }
+
+    companion object {
+        val interpolation = Interpolation.BILINEAR
+    }
+
+    enum class Interpolation {
+        NEAREST,
+        BILINEAR
     }
 }
