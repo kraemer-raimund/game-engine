dce2885b1e67ddb2329aecfa8ff62aa3cce57a82
Raimund Kraemer <kraemer_raimund@web.de>
2024-05-30 12:10:28 +0200

    Merge two very similar shaders into one

--- Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/rendering/shaders/DefaultFragmentShader.kt
+++ Engine/src/main/kotlin/dev/rakrae/gameengine/graphics/rendering/shaders/DefaultFragmentShader.kt
@@ -1,6 +1,5 @@
 package dev.rakrae.gameengine.graphics.rendering.shaders
 
-import dev.rakrae.gameengine.graphics.Color
 import dev.rakrae.gameengine.graphics.rendering.pipeline.FragmentShader
 import dev.rakrae.gameengine.graphics.rendering.pipeline.InputFragment
 import dev.rakrae.gameengine.graphics.rendering.pipeline.OutputFragment
@@ -9,7 +8,7 @@ class DefaultFragmentShader : FragmentShader {
 
     override fun process(inputFragment: InputFragment): OutputFragment {
         return OutputFragment(
-            fragmentColor = Color.white,
+            fragmentColor = inputFragment.material.color,
             depth = inputFragment.fragPos.z
         )
     }
